46c955d7d7a568b85543aef16d0d76e98b94c2ee
rosen777 <rosen.toshev@flatironschool.com>
2019-02-02 21:48:55 +0000

    Sample commit

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
-  def profile
-    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  def index
+    @users = User.all
   end
 
   def create
@@ -13,6 +13,24 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
+  end
+
+    def show
+        @user = User.find(params[:id])
+        render json: {user_details: @user}
+    end
+
+    def update
+        @user.update(user_params)
+        if @user.save
+         render json: @user, status: :accepted
+        else
+         render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
 
   private
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,5 +1,9 @@
-15.times do
-  User.create(
-    username: Faker::Friends.character, password: 'hi', avatar: Faker::Fillmurray.image, bio: Faker::Friends.quote
-  )
+# 15.times do
+#   User.create(
+#     username: Faker::Friends.character, password: 'hi', avatar: Faker::Fillmurray.image, bio: Faker::Friends.quote
+#   )
+
+User.create(username: 'test', password: '123')
+
+
 end

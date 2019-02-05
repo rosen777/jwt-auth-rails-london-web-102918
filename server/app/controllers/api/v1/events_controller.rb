27ec301630569762467d0140bdde406f8f9a7ff8
rosen777 <rosen.toshev@flatironschool.com>
2019-02-05 16:43:23 +0000

    The event controller has render json: @events added in the index method

--- server/app/controllers/api/v1/events_controller.rb
+++ server/app/controllers/api/v1/events_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::EventsController < ApplicationController
+
+    def index
+        @events = Event.all
+        render json: @events
+    end
+
     def create
         @event = Event.new(title: params[:title], capacity: params[:capacity], image: params[:image], date: params[:date],latitude: params[:latitude], longitude: params[:longitude], user_id: current_user.id )
 
@@ -8,4 +14,10 @@ class Api::V1::EventsController < ApplicationController
             render json: {error: 'Unable to create event.'}, status: 400
         end
     end
-end
+
+    def show
+        @event = Event.find(params[:id])
+        render json: {event_details: @event}
+    end
+
+end 
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
-      resources :events, only: [:create]
+      resources :events, only: [:index, :show, :create]
       post 'signin', to: 'users#signin'
       get 'validate', to: 'users#validate'
       get 'joined', to: 'users#get_events'
